b38bee67389366f307477c52e02ce2a0da05033b
eylonshm <eylonshm@gmail.com>
2022-11-30 19:39:34 +0200

    Add default state for the Outlet

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import ErrorPage from './errorPage'
 import Contact, { loader as contactLoader } from './routes/contact'
 import EditContact, { action as editAction } from './routes/edit'
 import { action as destroyAction } from './routes/destroy'
+import Index from './routes/index'
 
 const router = createBrowserRouter([
   {
@@ -16,6 +17,7 @@ const router = createBrowserRouter([
     loader: rootLoader,
     action: rootAction,
     children: [
+      { index: true, element: <Index /> },
       {
         path: 'contacts/:contactId',
         loader: contactLoader,
